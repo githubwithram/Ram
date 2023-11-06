fc95e183c28c012475147edfe07da8f42d18a893
rams git practice <shoppings.reddy@gmail.com>
2023-11-06 11:34:08 +0530

    Adding EmailSending to byrsh project

--- byresh/EmailSending.java
+++ byresh/EmailSending.java
@@ -1,13 +1,53 @@
-public class hello{
-	
-	public staticvoid main(string[] args){
-		
-		for(int i=1;i<=15;i++){
+package org.jdbc.maven.SendingMail;
 
-			System.out.println("Hello Aadvi"+i);
+import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailSending {
+
+	public static void main(String[] args) throws MessagingException {
+		String from = "exams.reddy@gmail.com";
+		String to = "krsr6666@gmail.com";
+		String subject = "ind vs eng match";
+		String text = "india won by 100 runs";
+
+		String host = "smtp.gmail.com";
+
+		Properties properties = System.getProperties();
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.ssl.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+
+		Session session = Session.getInstance(properties, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				// TODO Auto-generated method stub
+				return new PasswordAuthentication("exams.reddy@gmail.com", "dgus stuw wsvg duvi");
 			}
 
-		}
+		});
 
-	}	
+		session.setDebug(true);
+
+		MimeMessage message = new MimeMessage(session);
+		message.setFrom(new InternetAddress(from));
+		message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+		message.setSubject(subject);
+		message.setText(text);
+
+		Transport.send(message);
+		System.out.println("India is now top of the table with6 consecutive wins");
+
+	}
+
+}
